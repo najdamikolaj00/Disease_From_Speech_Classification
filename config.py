7f4a1b53d16bc6e91c197f95cea3f8fb1953e5e9
Tesla2000 <fratajczak124@gmail.com>
2023-12-12 22:21:01 +0100

    improved model saving

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ from torch.utils.tensorboard import SummaryWriter
 root_path = Path(".")
 data_path = root_path / "Data"
 session_time = datetime.now().strftime("%Y%m%d%H%M")
-results_folder = data_path.joinpath(f"results/{session_time}")
+results_folder = data_path.joinpath("results")
 results_folder.mkdir(exist_ok=True, parents=True)
 summary_folder = data_path.joinpath(f"summaries/{session_time}")
 summary_folder.mkdir(exist_ok=True, parents=True)
